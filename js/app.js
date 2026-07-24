6e6fe9a6720a93f034fe617e865eaffd04d0c406
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-07-24 22:36:57 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -172,10 +172,8 @@ function apply3DTilt(selector) {
       const rect = card.getBoundingClientRect();
       const x = e.clientX - rect.left;
       const y = e.clientY - rect.top;
-
       const centerX = rect.width / 2;
       const centerY = rect.height / 2;
-
       const rotateX = -(y - centerY) / 12;
       const rotateY = (x - centerX) / 12;
 
